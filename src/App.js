a60e5051394e7b77686cc3266eb3de402756a9a0
Fabricio Xavier Chagas <fabricio.chagas@bhs.com.br>
2019-12-21 23:50:33 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Main from './pages/main';
+
+import './styles.css';
+import Routes from './routes';
+
+const App = () => (
+      <div className="App">
+        <Header />
+        <Routes />
+      </div>
+);
+
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">jsHunt</header>
+);
+
+export default Header;
